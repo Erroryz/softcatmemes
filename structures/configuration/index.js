1cc571524502edba6ed7846515adc2bfff358a52
Error <129003249+Erroryz@users.noreply.github.com>
2023-12-25 17:04:48 +0000

    Update index.js

--- structures/configuration/index.js
+++ structures/configuration/index.js
@@ -5,5 +5,7 @@ module.exports = {
     mongodbURL: "",
     logsError: "", 
     developers: ["942485584184770630"],
+    uploadChannel: "", // upload's channel
+    databaseChannel: "", // steal discord storage lmao
     database: true,
 }
